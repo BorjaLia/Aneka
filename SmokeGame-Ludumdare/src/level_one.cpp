e661331f221048a0dd6ff8a3f974a153746eba99
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-04-18 18:37:38 -0300

    Separate files in .h

--- SmokeGame-Ludumdare/src/level_one.cpp
+++ SmokeGame-Ludumdare/src/level_one.cpp
@@ -1,21 +1,10 @@
 #pragma once
 
-#include "core/application.h"
-#include "core/scenes/Scene.h"
-#include "core/scenes/SceneManager.h"
-
-#include "core/types.h"
-
-#include "core/components/uiAnchorComponent.h"
-#include "core/components/textComponent.h"
+#include "level_one.h"
 
 #include "../src/prefabs/level_prefab.h"
 
-class LevelOne : public Engine::IScene
+void LevelOne::Build(Engine::SceneBuilder& builder) 
 {
-public:
-    void Build(Engine::SceneBuilder& builder) override
-    {
-        StartLevel(builder);
-    }
-};
+	StartLevel(builder);
+}
