177ee9fc9fce91ad5d0611b85dc44a7614e0d49f
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-04-19 00:23:40 -0300

    Add grid to level

--- SmokeGame-Ludumdare/src/level_one.cpp
+++ SmokeGame-Ludumdare/src/level_one.cpp
@@ -5,6 +5,7 @@
 
 #include "../src/prefabs/level_prefab.h"
 #include "../src/prefabs/obstacle_prefab.h"
+#include "../src/prefabs/grid_prefab.h"
 
 void LevelOne::Build(Engine::SceneBuilder& builder)
 {
@@ -15,5 +16,6 @@ void LevelOne::Build(Engine::SceneBuilder& builder)
 
 	Engine::Vector2f center = app.GetWindow()->GetSize() / 2;
 
+	AddGrid(builder, Engine::Vector2f(100.f,200.f), Engine::Vector2f(100.f,100.f), Engine::Vector2f(15,6));
 	AddObstacle(builder, "obsacle_one", rm.GetTexture("res/sprites/plant2.png"), center - Engine::Vector2f(50.0f, 10.0f), Engine::Vector2f(200.0f, 200.0f), true);
 }
