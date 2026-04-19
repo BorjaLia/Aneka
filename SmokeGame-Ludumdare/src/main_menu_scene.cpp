e412f690fef134500a9eaca71e8e2d418e8a18f1
elu <eluujazmin@gmail.com>
2026-04-19 00:49:45 -0300

    add cursor prefab

--- SmokeGame-Ludumdare/src/main_menu_scene.cpp
+++ SmokeGame-Ludumdare/src/main_menu_scene.cpp
@@ -13,6 +13,7 @@
 
 #include "background.h"
 #include "button.h"
+#include "cursor.h"
 
 void MainMenuScene::Build(Engine::SceneBuilder& builder)
 {
@@ -23,18 +24,23 @@ void MainMenuScene::Build(Engine::SceneBuilder& builder)
 
 	AddBackground(builder);
 
-	/*Engine::Node* camNode = */builder.CreateNode("MenuCamera");
+	Engine::Vector2f center = app.GetWindow()->GetSize() / 2;
+
+	Engine::Node* camNode = builder.CreateNode("MenuCamera");
+	camNode->transform->SetPosition(center);
+	auto* cam = camNode->AddComponent<Engine::CameraComponent>();
 
 	Engine::Texture2D titleTex = rm.GetTexture("res/sprites/aneka.png");
 
 	Engine::Node* title = builder.CreateNode("TitleText");
 	auto* sprite = title->AddComponent <Engine::SpriteComponent>(titleTex, Engine::Pivot::Center, Engine::Color(255, 255, 255, 255), Engine::RenderLayer::UI);
 	sprite->SetTargetSize({ 750.0f, 600.0f });
-	Engine::Vector2f center = app.GetWindow()->GetSize() / 2;
 
 	title->transform->SetPosition(center + Engine::Vector2f(0.0f, -230.0f));
 
 	AddButton(builder, center, "Play", "PlayButton", Engine::Vector2f(0.2f, 0.2f), rm.GetTexture("res/sprites/hoveredButton.png"), rm.GetTexture("res/sprites/button.png"), 15.0f);
 	AddButton(builder, center + Engine::Vector2f(0.0f, 150.0f), "Credits", "CreditsButton", Engine::Vector2f(0.2f, 0.2f), rm.GetTexture("res/sprites/hoveredButton.png"), rm.GetTexture("res/sprites/button.png"), -15.0f);
 	AddButton(builder, center + Engine::Vector2f(0.0f, 300.0f), "Exit", "ExitButton", Engine::Vector2f(0.2f, 0.2f), rm.GetTexture("res/sprites/hoveredButton.png"), rm.GetTexture("res/sprites/button.png"), 15.0f);
+
+	AddCursor(builder, cam);
 }
