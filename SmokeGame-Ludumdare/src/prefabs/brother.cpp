0a84ade8d32124b599a71c77b38e969826a8a0ef
elu <eluujazmin@gmail.com>
2026-04-18 23:31:51 -0300

    Merge

--- SmokeGame-Ludumdare/src/prefabs/brother.cpp
+++ SmokeGame-Ludumdare/src/prefabs/brother.cpp
@@ -4,6 +4,7 @@
 #include "../src/core/components/animatedSpriteComponent.h"
 #include "../src/core/components/colliderComponent.h"
 #include "../src/core/components/scriptComponent.h"
+
 #include "brother_script.h"
 
 void AddBrother(Engine::SceneBuilder& builder, Engine::Vector2f pos)
@@ -11,29 +12,23 @@ void AddBrother(Engine::SceneBuilder& builder, Engine::Vector2f pos)
 	auto& app = Engine::Application::Get();
 	auto& rm = *app.GetResourceManager();
 
-	const Engine::Texture2D walkingAnimText = rm.GetTexture("res/sprites/brotherSheet.png");
+    Engine::Texture2D brother_sptite_sheet = rm.GetTexture("res/sprites/brother/sprite_sheet.png");
 
 	Engine::Node* brother = builder.CreateNode("Brother");
 
-	Engine::AnimatedSpriteComponent* animations = brother->AddComponent<Engine::AnimatedSpriteComponent>(
-		walkingAnimText,
-		Engine::Pivot::TopLeft,
-		Engine::Color(255, 255, 255, 255),
-		Engine::RenderLayer::World
-	);
-	
-	animations->AddAnimationGrid("Walk", 0, 0, 8, 520, 819, 0.135f, true);
-	//animations->AddAnimationGrid("Idle", 0, 0, 8, 682, 682, 0.135f, true);
-	
-	brother->AddComponent<Engine::ScriptComponent>(new BrotherScript);
-	brother->transform->SetPosition(pos);
-	brother->transform->SetScale(Engine::Vector2f(0.3f, 0.3f));
+    brother->transform->SetPosition(pos);
 
-	//Engine::SpriteComponent* sprite = brother->AddComponent<Engine::SpriteComponent>
-	//    (
-	//        walkingAnimText,
-	//        Engine::Pivot::TopLeft,
-	//        Engine::Color(255,255,255,255),
-	//        Engine::RenderLayer::World
-	//    );
+    auto* spriteNode = builder.CreateChildNode(brother,"TextureNode");
+
+    spriteNode->transform->SetScale(Engine::Vector2f(0.2f,0.2f));
+
+    auto* sprite = spriteNode->AddComponent<Engine::AnimatedSpriteComponent>(brother_sptite_sheet,Engine::Pivot::Center);
+
+    sprite->AddAnimationGrid("Walk",0,0,8,670,1100,0.125f);
+
+	brother->AddComponent<Engine::ScriptComponent>(new BrotherScript);
+
+    //sprite->Play("Walk");
+
+    brother->AddComponent<Engine::ColliderComponent>(Engine::RectangleShape(Engine::Vector2f(100.0f,250.0f)));
 }
